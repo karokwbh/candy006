85411d5cd2ebac408ec454052a63a5076caf855f
Frank Huang <karokwbh2000@gmail.com>
2023-03-12 21:35:18 +0800

    answer

--- app.js
+++ app.js
@@ -3,7 +3,16 @@
 // 題目：找出在數字陣列裡跟其它元素不一樣的值
 
 function findDifferent(numbers) {
-    // 實作寫在這裡
+    if (!numbers || numbers.length <= 2) {
+        return;
+    }
+
+    numbers.sort((a, b) => a - b);
+    if (numbers[0] != numbers[1]) {
+        return numbers[0];
+    }
+
+    return numbers[numbers.length - 1];
 }
 
 console.log(findDifferent([1, 1, 1, 1, 3, 1, 1, 1])) // 印出 3
